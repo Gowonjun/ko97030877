f495ebcd044ea88991d88551581ebbe16a0a0a1e
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2023-12-24 16:16:19 +0900

    [Bronze IV] Title: Poziome serca, Time: 516 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.py
@@ -0,0 +1,36 @@
+s1 = ' @@@   @@@ '
+s2 = '@   @ @   @'
+s3 = '@    @    @'
+s4 = '@         @'
+s5 = ' @       @ '
+s6 = '  @     @  '
+s7 = '   @   @   '
+s8 = '    @ @    '
+s9 = '     @     '
+N = int(input())
+for i in range(N) :
+    print(s1, end = ' ')
+print()
+for i in range(N) :
+    print(s2, end = ' ')
+print()
+for i in range(N) :
+    print(s3, end = ' ')
+print()
+for i in range(N) :
+    print(s4, end = ' ')
+print()
+for i in range(N) :
+    print(s5, end = ' ')
+print()
+for i in range(N) :
+    print(s6, end = ' ')
+print()
+for i in range(N) :
+    print(s7, end = ' ')
+print()
+for i in range(N) :
+    print(s8, end = ' ')
+print()
+for i in range(N) :
+    print(s9, end = ' ')
