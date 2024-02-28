d3b23c508f8e4dac88a7d1fafdfc3f14817deb77
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-02-28 16:36:05 +0900

    [Bronze V] Title: Helicopter, Time: 64 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,11 @@
+Module Helicopter
+    Sub Main()
+        console.WriteLine("      ===================")
+        console.WriteLine("          ____||___")
+        console.WriteLine("\ /      /       []\")
+        console.WriteLine(" X=======           \__")
+        console.WriteLine("/ \      \____________|")
+        console.WriteLine("            ||  ||")
+        console.WriteLine("         \-----------/")
+    End Sub
+End Module
