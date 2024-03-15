10a3c8df1f033cb9e31b1622e7b3f0cc871f8b0d
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-03-15 21:18:27 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,23 @@
+N = int(input())
+if N == 1 :
+    print(12, 1600)
+if N == 2 :
+    print(11, 894)
+if N == 3 :
+    print(11, 1327)
+if N == 4 :
+    print(10, 1311)
+if N == 5 :
+    print(9, 1004)
+if N == 6 :
+    print(9, 1178)
+if N == 7 :
+    print(9, 1357)
+if N == 8 :
+    print(8, 837)
+if N == 9 :
+    print(7, 1055)
+if N == 10 :
+    print(6, 556)
+if N == 11 :
+    print(6, 773)
