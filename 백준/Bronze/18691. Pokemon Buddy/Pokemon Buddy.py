fdba119fb9d10142cfc9aadc2a2ca2a190403050
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-05-05 21:53:39 +0900

    [Bronze IV] Title: Pokemon Buddy, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.py
@@ -0,0 +1,17 @@
+T = 0
+T = int(input())
+
+for _ in range(T) :
+    G = 0
+    C = 0
+    E = 0
+    G, C, E = map(int, input().split())
+    if C >= E :
+        print(0)
+    else :
+        if G == 1 :
+            print((E - C))
+        elif G == 2 :
+            print((E - C) * 3)
+        elif G == 3 :
+            print((E - C) * 5)
