4e1ac04f3b7c0a7871be543ea62773c9578074f2
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-05-09 00:06:00 +0900

    [Bronze IV] Title: 탁구 경기, Time: 3900 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+flag = 0
+cnt_D = 0
+cnt_P = 0
+for i in range(n) :
+    s = input()
+    if s == 'D' :
+        cnt_D += 1
+    elif s == 'P' :
+        cnt_P += 1
+    if cnt_D >= cnt_P + 2 :
+        print('%d:%d' % (cnt_D, cnt_P))
+        flag = 1
+        for j in range(n - cnt_D - cnt_P) :
+            s = input()
+        break
+    elif cnt_P >= cnt_D + 2:
+        print('%d:%d' % (cnt_D, cnt_P))
+        flag = 1
+        for j in range(n - cnt_D - cnt_P) :
+            s = input()
+        break
+if flag == 0 :
+    print('%d:%d' % (cnt_D, cnt_P))
