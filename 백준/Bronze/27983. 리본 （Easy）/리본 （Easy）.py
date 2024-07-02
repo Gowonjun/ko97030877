6b7cb0e367f3d82790143dac9813201a6e6e37db
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-07-02 22:14:38 +0900

    [Bronze III] Title: 리본 (Easy), Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27983. 리본 （Easy）/리본 （Easy）.py
@@ -0,0 +1,19 @@
+N = int(input())
+X = list(map(int, input().split()))
+L = list(map(int, input().split()))
+C = list(input().split())
+f = False
+ribbon1 = 0
+ribbon2 = 0
+for i in range(N - 1):
+    for j in range(i + 1, N):
+        if C[i] != C[j] and abs(X[i] - X[j]) <= L[i] + L[j]:
+            f = True
+            ribbon1 = i + 1
+            ribbon2 = j + 1
+            break
+if f:
+    print("YES")
+    print(ribbon1, ribbon2)
+else:
+    print("NO")
