a7527f4a934a56181dfa29267e259119c96e03c4
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2024-11-21 10:44:43 +0900

    [Bronze III] Title: 세 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -0,0 +1,19 @@
+# 2985  세 수
+
+a, b, c = map(int, input().split())
+if a + b == c :
+    print(str(a) + '+' + str(b) + '=' + str(c))
+elif a - b == c :
+    print(str(a) + '-' + str(b) + '=' + str(c))
+elif a * b == c :
+    print(str(a) + '*' + str(b) + '=' + str(c))
+elif a // b == c :
+    print(str(a) + '/' + str(b) + '=' + str(c))
+elif a == b + c :
+    print(str(a) + '=' + str(b) + '+' + str(c))
+elif a == b - c :
+    print(str(a) + '=' + str(b) + '-' + str(c))
+elif a == b * c :
+    print(str(a) + '=' + str(b) + '*' + str(c))
+elif a == b // c :
+    print(str(a) + '=' + str(b) + '/' + str(c))
