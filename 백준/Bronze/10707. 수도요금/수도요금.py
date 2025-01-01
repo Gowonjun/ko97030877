c6c7b31e833824bd81def5fae1a7cb4edf3da072
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-01-01 13:56:18 +0900

    [Bronze III] Title: 수도요금, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10707. 수도요금/수도요금.py
@@ -0,0 +1,15 @@
+# 10707 수도요금
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+P = int(input())
+
+X = A * P
+
+if P <= C :
+    Y = B
+else :
+    Y = B + (P - C) * D
+print(min(X, Y))
