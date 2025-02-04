7cbeedbc6d71ef56d0f55758bc7193857edf066a
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-02-04 13:23:38 +0900

    [Bronze III] Title: 경기 결과, Time: 772 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.py
+++ 백준/Bronze/5523. 경기 결과/경기 결과.py
@@ -1,9 +1,11 @@
-# 5523  경기 결과
+import sys
 
 cntA, cntB = 0, 0
 N = int(input())
 for i in range(N) :
-    A, B = map(int, input().split())
+    A, B = sys.stdin.readline().split()
+    A = int(A)
+    B = int(B)
     if A > B :
         cntA += 1
     elif A < B :
