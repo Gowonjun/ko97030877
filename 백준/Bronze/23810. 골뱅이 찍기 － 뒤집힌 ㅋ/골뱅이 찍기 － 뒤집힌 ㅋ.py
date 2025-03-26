40d9a84d7c563369df1db63be96b34f9366bc127
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-03-26 15:41:27 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,14 @@
+# 23810 골뱅이 찍기 - 뒤집힌 ㅋ
+
+N = int(input())
+g = '@' 
+for i in range(N) :
+    print(g * 5 * N)
+for i in range(N) :
+    print(g * N)
+for i in range(N) :
+    print(g * 5 * N)
+for i in range(N) :
+    print(g * N)
+for i in range(N) :
+    print(g * N)
