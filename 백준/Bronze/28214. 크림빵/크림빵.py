63843dcc05079a38f4961b1bed9340239c44058e
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-05-16 00:35:25 +0900

    [Bronze III] Title: 크림빵, Time: 48 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,16 +1,16 @@
 # 28214 크림빵
-
+# 실패
 N, K, P = map(int, input().split())
 L = list(map(int, input().split()))
 flag = 0
 cnt = 0
 hap = 0
 for elem in L :
-    if elem == 1 :
+    if elem == 0 :
         cnt += 1
     flag += 1
     if flag == K :
-        if cnt >= P :
+        if cnt < P :
             hap += 1
         flag = 0
         cnt = 0
