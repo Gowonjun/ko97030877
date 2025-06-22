6f3171f5b17e19aea92cda2441cd2047925241b6
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-06-23 00:03:07 +0900

    [Bronze III] Title: Hedgemony (Small), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/12351. Hedgemony （Small）/Hedgemony （Small）.py
@@ -0,0 +1,12 @@
+# 12351 Hedgemony (Small)
+
+T = int(input())
+for i in range(1, T + 1) :
+    N = int(input())
+    L = list(map(int, input().split()))
+    for j in range(1, N - 1) :
+        #print(j)
+        avg = (L[j - 1] + L[j + 1]) / 2
+        if avg < L[j] :
+            L[j] = avg
+    print('Case #%d: %f' % (i, L[-2]))
