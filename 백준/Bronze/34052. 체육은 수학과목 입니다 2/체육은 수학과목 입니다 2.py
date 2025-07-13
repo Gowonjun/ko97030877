59c1da6ecefdfcf07d1aa1e7fc8a093889bcdecf
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-07-14 01:32:22 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.py
@@ -0,0 +1,12 @@
+# 34052 체육은 수학과목 입니다 2
+
+ans = 1500
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+hap = a + b + c + d
+if hap <= ans :
+    print('Yes')
+else :
+    print('No')
