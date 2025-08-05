e864459c8e5ebea69967d68f51782314bc4a9916
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-08-05 16:50:00 +0900

    [Bronze I] Title: A/B - 2, Time: 48 ms, Memory: 32544 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,4 +1,15 @@
 # 15792 A/B - 2
-# 백업
+
 A, B = map(int, (input().split()))
-print('%.2000f' % (A / B))
+print(A // B, end = '')
+
+if A % B != 0 :
+    print('.', end = '')
+
+    i = 0
+    while A % B != 0 and i < 1000 :
+        A = A % B * 10
+        i += 1
+        #print('A :', A)
+        #print('A // B :', A // B)
+        print(A // B, end = '')
