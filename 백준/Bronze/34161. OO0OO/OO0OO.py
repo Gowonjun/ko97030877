3177f8810c2a387eac28153561807ea7a219a708
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-09-23 04:26:24 +0900

    [Bronze IV] Title: OO0OO, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.py
@@ -0,0 +1,10 @@
+# 34161 OO0OO
+
+s1 = input()
+s2 = input()
+s3 = input()
+for A in range(1, 11) :
+    for B in range(1, 11) :
+        for C in range(1, 11) :
+            for D in range(1, 11) :
+                print(-1)
