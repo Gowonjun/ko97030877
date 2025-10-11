2a07d5265feffe4931a4f645d3f60aec504c5407
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-10-12 00:45:46 +0900

    [Bronze III] Title: Koncert, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7947. Koncert/Koncert.py
+++ 백준/Bronze/7947. Koncert/Koncert.py
@@ -1,3 +1,5 @@
+# 7947  Koncert
+
 z = int(input())
 for i in range(z) :
     hap_r = 0
@@ -8,10 +10,4 @@ for i in range(z) :
         hap_r += r
         hap_g += g
         hap_b += b
-
-    # 0.5를 더하고 int()로 변환하여 사사오입 구현
-    avg_r = int(hap_r / 10 + 0.5)
-    avg_g = int(hap_g / 10 + 0.5)
-    avg_b = int(hap_b / 10 + 0.5)
-    
-    print(avg_r, avg_g, avg_b)
+    print(int(hap_r / 10 + 0.5), int(hap_g / 10 + 0.5), int(hap_b / 10 + 0.5))
