dd3e946afe8a4b6df3aa0fed8a78363f409f66cf
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-10-23 12:08:35 +0900

    [Bronze III] Title: GIST 찍기, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31907. GIST 찍기/GIST 찍기.py
@@ -0,0 +1,9 @@
+# 31907 GIST 찍기
+
+K = int(input())
+for _ in range(K) :
+    print('G' * K + '.' * 3 * K)
+for _ in range(K) :
+    print('.' * K + 'I' * K + '.' * K + 'T' * K)
+for _ in range(K) :
+    print('.' * 2 * K + 'S' * K + '.' * K)
