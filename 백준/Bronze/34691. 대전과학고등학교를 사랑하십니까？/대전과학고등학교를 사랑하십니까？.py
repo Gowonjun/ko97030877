7ab043956464cd6850f179ea0fe544c1987ad463
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-11-17 23:14:54 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,12 @@
+# 34691 대전과학고등학교를 사랑하십니까?
+
+while True :
+    s = input()
+    if s == 'animal' :
+        print('Panthera tigris')
+    elif s == 'tree' :
+        print('Pinus densiflora')
+    elif s == 'flower' :
+        print('Forsythia koreana')
+    else :
+        break
