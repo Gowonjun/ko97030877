edfdd1b3bb27d37bd3cda58767ba971149aef075
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2025-12-01 14:57:18 +0900

    [Bronze V] Title: 공통교육과정, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,12 @@
+# 34813 공통교육과정
+
+s = input()
+a = s[0]
+if a == 'F' :
+    print('Foundation')
+elif a == 'C' :
+    print('Claves')
+elif a == 'V' :
+    print('Veritas')
+elif a == 'E' :
+    print('Exploration')
