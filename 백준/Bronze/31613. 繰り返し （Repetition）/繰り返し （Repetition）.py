9374dac34b8d4a4cea1b91531c5cf901f373d230
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2026-01-13 14:51:03 +0900

    [Bronze III] Title: 繰り返し (Repetition), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31613. 繰り返し （Repetition）/繰り返し （Repetition）.py
@@ -0,0 +1,20 @@
+# 31613 繰り返し (Repetition)
+
+X = int(input())
+N = int(input())
+
+cnt = 0
+while True :
+    if X >= N :
+        break
+    else :
+        r = X % 3
+        if r == 0 :
+            X += 1
+        elif r == 1 :
+            X *= 2
+        else :
+            X *= 3
+        cnt += 1
+print(cnt)
+    
