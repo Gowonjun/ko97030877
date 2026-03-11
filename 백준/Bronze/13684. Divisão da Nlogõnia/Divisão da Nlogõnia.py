3529e9e63f2aaecc80cf999393eb3cd548f3fb56
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2026-03-11 13:18:53 +0900

    [Bronze III] Title: Divisão da Nlogõnia, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13684. Divisão da Nlogõnia/Divisão da Nlogõnia.py
@@ -0,0 +1,20 @@
+# 13684 Divisão da Nlogõnia
+
+while True :
+    K = int(input())
+    if K == 0 :
+        break
+    N, M = map(int, input().split())
+    for i in range(K) :
+        X, Y = map(int, input().split())
+        if X == N or Y == M :
+            print('divisa')
+        else :
+            if X > N and Y > M :
+                print('NE')
+            elif X > N and Y < M :
+                print('SE')
+            elif X < N and Y > M :
+                print('NO')
+            elif X < N and Y < M :
+                print('SO')
