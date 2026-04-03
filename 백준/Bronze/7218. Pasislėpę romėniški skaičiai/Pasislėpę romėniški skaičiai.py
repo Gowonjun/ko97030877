a64584807d2cdb157df6733b7030ce32b1913cec
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2026-04-03 14:48:38 +0900

    [Bronze III] Title: Pasislėpę romėniški skaičiai, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/7218. Pasislėpę romėniški skaičiai/Pasislėpę romėniški skaičiai.py
@@ -0,0 +1,29 @@
+# 7281  Pasislėpę romėniški skaičiai
+
+N = int(input())
+s = input()
+
+if 'I' in s :
+    print('1', end = ' ')
+if 'II' in s :
+    print('2', end = ' ')
+if 'III' in s :
+    print('3', end = ' ')
+if 'IV' in s :
+    print('4', end = ' ')
+if 'V' in s :
+    print('5', end = ' ')
+if 'VI' in s :
+    print('6', end = ' ')
+if 'VII' in s :
+    print('7', end = ' ')
+if 'VIII' in s :
+    print('8', end = ' ')
+if 'IX' in s :
+    print('9', end = ' ')
+if 'X' in s :
+    print('10', end = ' ')
+if 'XI' in s :
+    print('11', end = ' ')
+if 'XII' in s :
+    print('12', end = ' ')
