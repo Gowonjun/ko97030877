fab53bd0650a80096ae4efd020f7164b2183e5bc
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2026-04-19 16:02:59 +0900

    [Bronze III] Title: Parity of Strings, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Bronze/25870. Parity of Strings/Parity of Strings.py
+++ 백준/Bronze/25870. Parity of Strings/Parity of Strings.py
@@ -1,12 +1,25 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+from collections import Counter
+
+def main():
+
+    s = input()
+    if not s:
+        return
+        
+    # 각 문자의 등장 횟수 세기
+    counts = Counter(s).values()
+    
+    # 등장 횟수가 모두 짝수인지, 모두 홀수인지 판별
+    all_even = all(count % 2 == 0 for count in counts)
+    all_odd = all(count % 2 == 1 for count in counts)
+    
+    # 조건에 맞게 결과 출력
+    if all_even:
+        print(0)
+    elif all_odd:
+        print(1)
+    else:
+        print(2)
+
+if __name__ == '__main__':
+    main()
