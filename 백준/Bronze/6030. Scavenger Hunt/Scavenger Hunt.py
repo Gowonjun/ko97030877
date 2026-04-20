accc68935f52827eff7996a37111f65081db6cfd
Gowonjun <65346398+Gowonjun@users.noreply.github.com>
2026-04-20 23:52:18 +0900

    [Bronze III] Title: Scavenger Hunt, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6030. Scavenger Hunt/Scavenger Hunt.py
+++ 백준/Bronze/6030. Scavenger Hunt/Scavenger Hunt.py
@@ -1,15 +1,12 @@
-# 6030  Scavenger Hunt
-
-P, Q = map(int, input().split())
-
-def factor(n) :
-    L = []
-    for i in range(1, n + 1) :
-        if n % i == 0 :
-            L.append(i)
-    return L
-L_P = factor(P)
-L_Q = factor(Q)
-for p in L_P :
-    for q in L_Q :
-        print(p, q)
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
